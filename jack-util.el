ed63040858bb71eaf9d36ed1a1aeb3e3a350c1d5
jack <jacktasia@gmail.com>
2012-08-08 13:52:16 -0700

    disable scrolling too

--- jack-util.el
+++ jack-util.el
@@ -70,6 +70,7 @@
 				 [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
 				 [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
 				 [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+				 [wheel-down] [wheel-up]
 				 [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
 	  (global-unset-key k)
 	  (global-set-key k '(lambda()(interactive)(message "%s" "bad human!")))))
