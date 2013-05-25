30527de19456c2b5f38ba5ef16a5c122467b78c0
Jack <jacktasia@gmail.com>
2013-05-25 00:31:02 -0700

    python spaces

--- dotemacs24.el
+++ dotemacs24.el
@@ -220,4 +220,10 @@
 
 (message ".emacs loaded in %s seconds" (mapconcat 'int-to-string (rest (time-since *start-time*)) "."))
 
+(add-hook 'python-mode-hook
+  (lambda ()
+    (setq indent-tabs-mode t)
+    (setq python-indent 8)
+    (setq tab-width 4)))
+
 (provide 'dotemacs24)
