2ad5c777b4bcd187b7a8866a9f09138852a3a76e
jack <jacktasia@gmail.com>
2015-07-05 17:27:22 -0700

    git-gutter+ is fixed now

--- dotemacs24.el
+++ dotemacs24.el
@@ -268,7 +268,7 @@
 ;;
 ;; packages to install
 ;;
-(let ((pkgs-to-install '(company company-anaconda company-tern ace-jump-mode ace-jump-buffer fuzzy-match rainbow-delimiters php-mode go-mode web-mode multiple-cursors dash s projectile fringe-helper flycheck f ido-sort-mtime flx-ido switch-window anzu git-gutter git-gutter-fringe smex exec-path-from-shell groovy-mode ag highlight-symbol ws-butler ht smart-mode-line smart-mode-line-powerline-theme imgix fic-mode multi-term ido-vertical-mode dtrt-indent jsx-mode scss-mode helm helm-projectile)))
+(let ((pkgs-to-install '(company company-anaconda company-tern ace-jump-mode ace-jump-buffer fuzzy-match rainbow-delimiters php-mode go-mode web-mode multiple-cursors dash s projectile fringe-helper flycheck f ido-sort-mtime flx-ido switch-window anzu git-gutter+ git-gutter-fringe+ smex exec-path-from-shell groovy-mode ag highlight-symbol ws-butler ht smart-mode-line smart-mode-line-powerline-theme imgix fic-mode multi-term ido-vertical-mode dtrt-indent jsx-mode scss-mode helm helm-projectile)))
   ;; install the packages
   (jack-require-or-install-all pkgs-to-install))
 
@@ -353,9 +353,9 @@
 (global-set-key (quote [M-up]) (quote scroll-down-line))
 
 
-(global-git-gutter-mode t)
-(global-set-key (kbd "C-x g") 'git-gutter-mode)
-(global-set-key (kbd "C-x C-g") 'git-gutter-mode) ; because i accidentally do this half the time anyway
+(global-git-gutter+-mode t)
+(global-set-key (kbd "C-x g") 'git-gutter+-mode)
+(global-set-key (kbd "C-x C-g") 'git-gutter+-mode) ; because i accidentally do this half the time anyway
 
 (define-key global-map (kbd "C-c s") 'ace-jump-char-mode)
 (define-key global-map (kbd "C-s") 'isearch-forward)
