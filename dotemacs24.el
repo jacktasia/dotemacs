cdcefb27a56b6ca38add0cc83e0861dacc400f94
Jack <jacktasia@gmail.com>
2015-11-03 14:03:30 -0800

    no special font

--- dotemacs24.el
+++ dotemacs24.el
@@ -267,7 +267,7 @@
 (scroll-bar-mode -1)
 
 
-(set-default-font "Terminus-14")
+;(set-default-font "Terminus-14")
 
 ;; (when (member "Terminus (TTF)" (font-family-list))
 ;;   (set-face-attribute 'default nil :font "Terminus (TTF)"))
