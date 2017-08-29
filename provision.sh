296aab831a8002da31f012e06f55e38083ab3673
jack <jacktasia@gmail.com>
2017-08-29 10:54:31 -0700

    update vagrant provision script

--- provision.sh
+++ provision.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # setup keys
-ssh -o StrictHostKeyChecking=no git@github.com
-cp /vagrant/id_rsa /root/.ssh/id_rsa
-
 add-apt-repository ppa:kelleyk/emacs -y
 apt-get update
 apt-get install emacs25 -y
 
+cp /vagrant/id_rsa /root/.ssh/id_rsa
 
+su -l ubuntu
+cp /vagrant/id_rsa /home/ubuntu/.ssh/id_rsa
+chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa
+ssh -o StrictHostKeyChecking=no git@github.com
 mkdir -p /home/ubuntu/code
+chown -R ubuntu:ubuntu /home/ubuntu/code
 cd code
 git clone git@github.com:jacktasia/dotemacs.git
+chown -R ubuntu:ubuntu /home/ubuntu/code
 
 ln -s /home/ubuntu/code/dotemacs/dotemacs24.el /home/ubuntu/.emacs
